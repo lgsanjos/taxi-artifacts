108f2999060c88fbc6d2272990e4fe660c73f858
jenkins <jenkins@fasttaxi.com.br>
2015-11-17 12:13:36 -0500

    'adding sidekiq config for prod' Build #11 Nov 17, 2015 12:12:35

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,2 +1,25 @@
 require 'sidekiq'
+require 'yaml'
 Dir["./config/initializers/**.rb", "./app/**.rb", "./app/workers/**.rb", "./app/model/**.rb", "./app/services/**.rb"].each {|file| require file}
+
+config = YAML.load_file('./config/redis.yml')
+env = ENV['RACK_ENV'].nil? ? 'development' : ENV['RACK_ENV']
+properties = config[env]
+{"host"=>"localhost", "port"=>6379, "database"=>0}
+
+host = properties['host']
+port = properties['port']
+#namespace = properties[:namespace]
+db_num = properties['database']
+
+#Sidekiq.configure_server do |config|
+  #config.redis = { :host => "redis://#{host}:#{port}/#{db_num}", :namespace => namespace }
+#end
+
+Sidekiq.configure_server do |config|
+  config.redis = { :host => "redis://#{host}:#{port}/#{db_num}"}
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = { :host => "redis://#{host}:#{port}/#{db_num}"}
+end
